efeb7a7c376ca00e15cc02de2726859ba50838eb
Nicholas Serra <nicks@ydekproductions.com>
2016-04-07 14:41:28 -0400

    Page changes, js cleanup

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    var flickr_json_url = 'https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=fab8e8f200b71e58f52419b0d512df8d&user_id=14670946%40N04&per_page=12&page=1&format=json&nojsoncallback=1',
-        wordpress_json_url = 'http://tech.nickserra.com/api/get_recent_posts/?callback=?',
-        github_json_url = 'https://api.github.com/users/nicholasserra/repos?callback=ns';
+    var flickrUrl = 'https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=fab8e8f200b71e58f52419b0d512df8d&user_id=14670946%40N04&per_page=12&page=1&format=json&nojsoncallback=1',
+        blogUrl = 'http://tech.nickserra.com/api/get_recent_posts/?callback=?',
+        githubUrl = 'https://api.github.com/users/nicholasserra/repos?callback=ns&sort=pushed';
 
-    $.getJSON(flickr_json_url, function(data) {
+    $.getJSON(flickrUrl, function(data) {
       var items = [];
       $.each(data.photos.photo, function(item, val) {
-        items.push('<li><a href="http://farm'+val.farm+'.staticflickr.com/'+val.server+'/'+val.id+'_'+val.secret+'.jpg" title="'+val.title+'" rel="gallery"><img src="http://farm'+val.farm+'.staticflickr.com/'+val.server+'/'+val.id+'_'+val.secret+'_s.jpg" / alt="'+val.title+'"></a></li>');
+        items.push('<li><a href="https://www.flickr.com/photos/'+val.owner+'/'+val.id+'" title="'+val.title+'" rel="gallery"><img src="https://farm'+val.farm+'.staticflickr.com/'+val.server+'/'+val.id+'_'+val.secret+'_s.jpg" / alt="'+val.title+'"></a></li>');
       });
 
       $('<ul/>', {
@@ -16,23 +16,23 @@ $(document).ready(function(){
     });
 
     $.ajax({
-        url: github_json_url,
+        url: githubUrl,
         dataType:"jsonp",
         success: function(data) {
             var items = [];
             $.each(data.data, function(item, val) {
                 if (!val.fork){
-                    items.push('<li><a href="'+val.url+'">'+val.name+'</a></li>');
+                    items.push('<li><a href="'+val.html_url+'">'+val.name+'</a></li>');
                 }
             });
             $('<ul/>', {
               'class': 'nav nav-list',
-              html: '<li class="nav-header">Repositories</li>'+items.join('')
+              html: items.join('')
             }).appendTo('#repos');
         }
     });
 
-    $.getJSON(wordpress_json_url, function(data) {
+    $.getJSON(blogUrl, function(data) {
       var items = [];
       $.each(data.posts, function(item, val) {
         items.push('<li><a href="'+val.url+'">'+val.title+'</a></li>');
@@ -40,7 +40,7 @@ $(document).ready(function(){
 
       $('<ul/>', {
         'class': 'nav nav-list',
-        html: '<li class="nav-header">Blog Posts</li>'+items.join('')
+        html: items.join('')
       }).appendTo('#blogposts');
     });
-})
+});
